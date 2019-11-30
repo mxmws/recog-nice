b920578f68600994dd435964f3577b857b49f573
Minh <s0558411@htw-berlin.de>
2019-11-30 16:13:22 +0100

    Added correct project

--- VisualStudio_Projekt/PCD2ASC/PCD2ASC.cpp
+++ VisualStudio_Projekt/PCD2ASC/PCD2ASC.cpp
@@ -14,7 +14,7 @@
 #include <pcl/io/pcd_io.h>      // header that contains the definitions for PCD I/O operations
 #include <iterator>
 #include <math.h>
-
+#include <pcl/io/ply_io.h>
 
 using namespace std;
 
@@ -22,83 +22,102 @@ using namespace std;
 //int _tmain(int argc, _TCHAR* argv[])
 int main (int argc, char  *argv[])
 {
-	string sIn;
-	float line_stepsize = 3.0;
+//Stefans Testcode
+#pragma region Stefans Testcode
 
-	if (argc == 1)
-	{
-		cout << "Input File: ";
-		cin >> sIn;
-	}
-	else if (argc == 2)
-	{
-		sIn = (char*)&(*argv[1]);
-	}
-	else
-	{
-		cout << "Error in Command Line!" << endl;
-		cout << "usage: PCDtoASC.exe input-file" << endl;
-		return -1;
-	}
+	//string sIn;
+	//float line_stepsize = 3.0;
 
-	pcl::PointCloud<pcl::PointNormal>::Ptr cloud_ptr (new pcl::PointCloud<pcl::PointNormal>);
-	fstream file;
-	string s;
 
-	std::cout << std::endl << "Open file \'" << sIn << "\'" << std::endl;
+	//if (argc == 1)
+	//{
+	//	cout << "Input File: ";
+	//	cin >> sIn;
+	//}
+	//else if (argc == 2)
+	//{
+	//	sIn = (char*)&(*argv[1]);
+	//}
+	//else
+	//{
+	//	cout << "Error in Command Line!" << endl;
+	//	cout << "usage: PCDtoASC.exe input-file" << endl;
+	//	return -1;
+	//}
 
-	int res = pcl::io::loadPCDFile<pcl::PointNormal>(sIn.c_str(), *cloud_ptr);
+	//pcl::PointCloud<pcl::PointNormal>::Ptr cloud_ptr (new pcl::PointCloud<pcl::PointNormal>);
+	//fstream file;
+	//string s;
 
-	if (res == -1)
-	{
-		cout << "Unable to open PCD_File! Abort." << endl;
-		return -1;
-	}
+	//std::cout << std::endl << "Open file \'" << sIn << "\'" << std::endl;
 
-	string sOut;
-	std::size_t found = sIn.find(".");
-	std::size_t found_last;
-	if (found!=std::string::npos)
-	{
-		found_last = found;
-		found = sIn.find(".", found+1);
-		while(found!=std::string::npos)
-		{
-			found_last = found;
-			found = sIn.find(".", found+1);
-		}
-		sOut = sIn.substr (0,found_last) + ".asc";
-	}
-	else
-	{
-		sOut = sIn + ".asc";
-	}	
-	//	pcl::io::savePCDFileBinary(sOut, cloud);
-	file.open(sOut.c_str(), ios::out);
+	//int res = pcl::io::loadPCDFile<pcl::PointNormal>(sIn.c_str(), *cloud_ptr);
 
-	file << "# 3D ASCII (X Y Z)" << std::endl;
+	//if (res == -1)
+	//{
+	//	cout << "Unable to open PCD_File! Abort." << endl;
+	//	return -1;
+	//}
 
-	for (int i=0; i < (int)cloud_ptr->points.size(); i++)
-	{
-		//file << cloud.points[i].x <<"\t" << cloud.points[i].y << "\t" << cloud.points[i].z << std::endl;
-		file.width(11);
-		file << cloud_ptr->points[i].x << " ";
-		file.width(11);
-		file << cloud_ptr->points[i].y << " ";
-		file.width(11);
-		file << cloud_ptr->points[i].z << " ";
-		file.width(11);
-		file << cloud_ptr->points[i].normal_x << " ";
-		file.width(11);
-		file << cloud_ptr->points[i].normal_y << " ";
-		file.width(10);
-		file << cloud_ptr->points[i].normal_z << std::endl;
-	}
+	//string sOut;
+	//std::size_t found = sIn.find(".");
+	//std::size_t found_last;
+	//if (found!=std::string::npos)
+	//{
+	//	found_last = found;
+	//	found = sIn.find(".", found+1);
+	//	while(found!=std::string::npos)
+	//	{
+	//		found_last = found;
+	//		found = sIn.find(".", found+1);
+	//	}
+	//	sOut = sIn.substr (0,found_last) + ".asc";
+	//}
+	//else
+	//{
+	//	sOut = sIn + ".asc";
+	//}	
+	////	pcl::io::savePCDFileBinary(sOut, cloud);
+	//file.open(sOut.c_str(), ios::out);
 
-	file.close();
+	//file << "# 3D ASCII (X Y Z)" << std::endl;
 
-	std::cerr << endl << "Saved " << cloud_ptr->points.size () << " data points to " << sOut << endl;
+	//for (int i=0; i < (int)cloud_ptr->points.size(); i++)
+	//{
+	//	//file << cloud.points[i].x <<"\t" << cloud.points[i].y << "\t" << cloud.points[i].z << std::endl;
+	//	file.width(11);
+	//	file << cloud_ptr->points[i].x << " ";
+	//	file.width(11);
+	//	file << cloud_ptr->points[i].y << " ";
+	//	file.width(11);
+	//	file << cloud_ptr->points[i].z << " ";
+	//	file.width(11);
+	//	file << cloud_ptr->points[i].normal_x << " ";
+	//	file.width(11);
+	//	file << cloud_ptr->points[i].normal_y << " ";
+	//	file.width(10);
+	//	file << cloud_ptr->points[i].normal_z << std::endl;
+	//}
 
-	return 0;
+	//file.close();
+
+	//std::cerr << endl << "Saved " << cloud_ptr->points.size () << " data points to " << sOut << endl;
+
+	//return 0;
+#pragma endregion
+
+#pragma region PLY_READER
+	pcl::PointCloud<pcl::PointNormal>::Ptr cloud_ptr(new pcl::PointCloud<pcl::PointNormal>);
+
+	pcl::PLYReader Reader;
+
+	Reader.read("C:/Users/Minh/Desktop/repository/team5/RefModelle/DeoPLY.ply", *cloud_ptr);
+
+	string writePath = "C:/Users/Minh/Desktop/repository/team5/RefModelle/DeoKOPIE.ply";
+
+	pcl::io::savePLYFileBinary(writePath, *cloud_ptr);
+#pragma endregion
+
+
 }
 
