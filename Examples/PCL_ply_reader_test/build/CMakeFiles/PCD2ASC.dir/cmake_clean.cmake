40c0f1f96d391d6da7dd8b467d1855ee563f0570
Michael Reno <s0565907@htw-berlin.de>
2019-12-01 17:07:23 +0100

    Add .ply reader test data in example folder

---
+++ Examples/PCL_ply_reader_test/build/CMakeFiles/PCD2ASC.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PCD2ASC.dir/PCD2ASC.cpp.o"
+  "PCD2ASC"
+  "PCD2ASC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PCD2ASC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
