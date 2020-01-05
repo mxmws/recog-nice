5c6b3216a4897f3af4aab0ee7c3c094734129001
Michael Reno <s0565907@htw-berlin.de>
2020-01-05 15:43:18 +0100

    Removed unnecessary codes from class ReferenceModel

--- VisualStudio_Projekt/PCD2ASC/ReferenceModel.h
+++ VisualStudio_Projekt/PCD2ASC/ReferenceModel.h
@@ -1,13 +1,9 @@
 #include <pcl/io/ply_io.h>
-#include <string>
-using namespace std;
 
 #pragma once
 class ReferenceModel
 {
 public:
-	std::string filename;
-	ReferenceModel() = default;
 	ReferenceModel(pcl::PointCloud<pcl::PointXYZ>::Ptr);
 	float scoreSimilarity(pcl::PointCloud<pcl::PointXYZ>::Ptr);
 private:
