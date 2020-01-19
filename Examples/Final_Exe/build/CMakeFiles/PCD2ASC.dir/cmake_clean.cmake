b521f09377f2cba2a949dcd0d5716672a696373a
s0564319 <maximilian.mews@student.htw-berlin.de>
2020-01-19 15:16:06 +0100

    works on raspi now

---
+++ Examples/Final_Exe/build/CMakeFiles/PCD2ASC.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PCD2ASC.dir/home/pi/Desktop/repository/team5/VisualStudio_Projekt/PCD2ASC/PCD2ASC.cpp.o"
+  "CMakeFiles/PCD2ASC.dir/home/pi/Desktop/repository/team5/VisualStudio_Projekt/PCD2ASC/Processing.cpp.o"
+  "CMakeFiles/PCD2ASC.dir/home/pi/Desktop/repository/team5/VisualStudio_Projekt/PCD2ASC/ReferenceModel.cpp.o"
+  "PCD2ASC"
+  "PCD2ASC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PCD2ASC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
