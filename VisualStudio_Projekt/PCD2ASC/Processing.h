89fe6a9c8b708167ae1a6ff9f9c991e46ac125f6
Minh <s0558411@htw-berlin.de>
2020-01-19 14:13:22 +0100

    test

--- VisualStudio_Projekt/PCD2ASC/Processing.h
+++ VisualStudio_Projekt/PCD2ASC/Processing.h
@@ -15,7 +15,7 @@ public:
 	pcl::PointCloud<pcl::PointXYZ>::Ptr linuxPlyReader(string&);
 	//void Processing::plyWriter(string, pcl::PointCloud<pcl::PointXYZ>::Ptr);
 	pcl::PointCloud<pcl::PointXYZ>::Ptr removeBackground(pcl::PointCloud<pcl::PointXYZ>::Ptr);
-	vector<float>Processing::getRemovalParameters(pcl::PointCloud<pcl::PointXYZ>::Ptr);
+	vector<float>getRemovalParameters(pcl::PointCloud<pcl::PointXYZ>::Ptr);
 	pcl::PointCloud<pcl::PointXYZ>::Ptr uptRemoveBackground(pcl::PointCloud<pcl::PointXYZ>::Ptr,vector<float>);
 
 	pcl::PointCloud<pcl::PointXYZ>::Ptr extractGround(pcl::PointCloud<pcl::PointXYZ>::Ptr, pcl::PointCloud<pcl::PointXYZ>::Ptr);
