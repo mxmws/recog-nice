4e637e8cb518ef99358ac0f9134e8664f54c9bda
Maximilian <maximilian.mews@gmail.com>
2020-01-26 18:45:38 +0100

    Added comments. A lot of comments.

--- VisualStudio_Projekt/PCD2ASC/Processing.cpp
+++ VisualStudio_Projekt/PCD2ASC/Processing.cpp
@@ -32,13 +32,11 @@
 using namespace std;
 
 
-
-
 /**
 * Loading ply file into PointCloud object and return it.
 *
 * @param filename name of the file that is going to be opened
-* @return PointCloud::Ptr Returns a PointCloud object.
+* @return Returns a PointCloud object.
 *
 * Sources:	https://stackoverflow.com/questions/30764222/how-to-read-ply-file-using-pcl
 */
@@ -52,25 +50,34 @@ pcl::PointCloud<pcl::PointXYZ>::Ptr Processing::plyReader(string& filename)
 	return cloud_ptr;
 }
 
+
+/**
+* Loading ply file into PointCloud object and return it.
+*
+* @param sourceCloudFile name of the file that is going to be opened if on Windows
+* @return	On Windows:	file opened
+*			On Linux:	most recent scan
+*
+*/
 pcl::PointCloud<pcl::PointXYZ>::Ptr Processing::startScanning(string& sourceCloudFile)
 {
 	#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
-	
+	//open file to simulate a scan on windows
 	return plyReader(sourceCloudFile);
 	
 	#else
 
+	//make a scan
 	CRealsenseScan scan;
 	string filename = "halloDasIstUnserScan.ply";
-	
 	if (scan.performScanAndSave(filename) == false)
 	{
 		cout << "Plug in you camera first and hit enter again" << endl;
 		cin.get();
 		startScanning(sourceCloudFile);
 	}
-	
-	//system("/home/pi/librealsense/build/examples/pointcloud/rs-pointcloud");
+
+	//open the scan
 	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = plyReader(filename);
 
 	
@@ -82,6 +89,7 @@ pcl::PointCloud<pcl::PointXYZ>::Ptr Processing::startScanning(string& sourceClou
 			return cloud;
 		}
 	}
+	//recursion if scan was faulty
 	startScanning(sourceCloudFile);
 	
 	#endif
@@ -232,97 +240,102 @@ pcl::PointCloud<pcl::PointXYZ>::Ptr Processing::uptRemoveBackground
 }
 
 
+/**
+* Subtracts two pointclouds from each other to the flat ground. 
+*
+* @param source_cloud_1 for example the plain ground
+* @param source_cloud_2 for example objects placed on the ground marking the area that needs to be extracted from source_cloud_1
+*
+* @return extracted ground
+*
+* Sources: http://pointclouds.org/documentation/tutorials/octree.php
+* http://pointclouds.org/documentation/tutorials/planar_segmentation.php
+*/
 pcl::PointCloud<pcl::PointXYZ>::Ptr Processing::extractGround(pcl::PointCloud<pcl::PointXYZ>::Ptr source_cloud_1, pcl::PointCloud<pcl::PointXYZ>::Ptr source_cloud_2)
 {
-	cout << "extracting ground... (luckily this won't take 20 Minutes anymore)" << endl;
+	cout << "extracting ground..." << endl;
 
+	//using spacial partitioning to increase performance from pow(n, 2) to n*log(n)
 	float resolution = 0.1f;
-
 	pcl::octree::OctreePointCloudSearch<pcl::PointXYZ> octree(resolution);
-
 	octree.setInputCloud(source_cloud_2);
 	octree.addPointsFromInputCloud();
-
 	pcl::PointXYZ searchPoint;
-
 	std::vector<int> pointIdxNKNSearch;
 	std::vector<float> pointNKNSquaredDistance;
 
 	pcl::PointIndices::Ptr ToBeRemoved(new pcl::PointIndices());
 	pcl::ExtractIndices<pcl::PointXYZ> extract;
 
-
 	
-	//remove all points from source_cloud_1 that are similar in source_cloud_2
+	//remove all points from source_cloud_1 that match points in source_cloud_2
 	for (int i = 0; i < source_cloud_1->size(); i++)
 	{
 		searchPoint = source_cloud_1->points[i];
 
-		//Comparing points from both clouds with the same ID is twice as fast compared to octree and true in most cases
+		//Comparing points from both clouds with the same ID is way faster compared to octree and true in most cases
 		if(i < source_cloud_2->size())
 		{
 			pcl::PointXYZ pt2 = source_cloud_2->points[i];
-			
+
+			//measure distance between two points from both clouds with the same ID
 			if (sqrt(pow(searchPoint.x - pt2.x, 2) + pow(searchPoint.y - pt2.y, 2) + pow(searchPoint.z - pt2.z, 2)) < 0.05)
 			{
 				ToBeRemoved->indices.push_back(i);
 				continue;
 			}
 		}
-		
+
+		//search for nearest point in other cloud
 		octree.nearestKSearch(searchPoint, 1, pointIdxNKNSearch, pointNKNSquaredDistance);
 
-		if (pointNKNSquaredDistance[0]<0.002)//0.002
+		//check if close enough
+		if (pointNKNSquaredDistance[0]<0.002)
 		{
 			ToBeRemoved->indices.push_back(i);
 		}
 
 	}
 
+	//remove the points that are similar in both clouds
 	extract.setInputCloud(source_cloud_1);
 	extract.setIndices(ToBeRemoved);
 	//Remove points
 	extract.setNegative(true);
 	extract.filter(*source_cloud_1);
 
-	/*
-	pcl::RadiusOutlierRemoval<pcl::PointXYZ> rorfilter(true);
-	rorfilter.setInputCloud(source_cloud_1);
-	rorfilter.setRadiusSearch(0.4);//0.1
-	rorfilter.setMinNeighborsInRadius(100);//10
-	rorfilter.filter(*source_cloud_1);
-	*/
-
-
+	//determine a plane from the points left using plane model segmentation
 	pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
 	pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
 	// Create the segmentation object
 	pcl::SACSegmentation<pcl::PointXYZ> seg;
-	// Optional
 	seg.setOptimizeCoefficients(true);
-	// Mandatory
 	seg.setModelType(pcl::SACMODEL_PLANE);
 	seg.setMethodType(pcl::SAC_RANSAC);
 	seg.setDistanceThreshold(0.01);
-
 	seg.setInputCloud(source_cloud_1);
 	seg.segment(*inliers, *coefficients);
 
-
+	//remove points that are not part of the plane
 	pcl::ExtractIndices<pcl::PointXYZ> extract2;
-
 	extract2.setInputCloud(source_cloud_1);
 	extract2.setIndices(inliers);
 	//Remove points
 	extract2.setNegative(false);
 	extract2.filter(*source_cloud_1);
 
-	
 	cout << "Done..." << endl;
 	return source_cloud_1;
 }
 
 
+/**
+* Determines the angle of a pointcloud that is flat like a plane
+*
+* @param source_cloud Takes a PointCloud object, preferably a flat one.
+*
+* Sources: http://pointclouds.org/documentation/tutorials/normal_estimation.php
+*/
 void Processing::determineAngle(pcl::PointCloud<pcl::PointXYZ>::Ptr source_cloud)
 {
 	cout << "determineAngle..." << endl;
@@ -334,44 +347,40 @@ void Processing::determineAngle(pcl::PointCloud<pcl::PointXYZ>::Ptr source_cloud
 	// Its content will be filled inside the object, based on the given input dataset (as no other search surface is given).
 	pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZ>());
 	ne.setSearchMethod(tree);
-
+	
 	// Output datasets
 	pcl::PointCloud<pcl::Normal>::Ptr cloud_normals(new pcl::PointCloud<pcl::Normal>);
-
 	// Use all neighbors in a sphere of radius 3cm
 	ne.setRadiusSearch(0.03);
-
 	// Compute the features
-	ne.compute(*cloud_normals);//dis dont work idk why
+	ne.compute(*cloud_normals);
 
 
 	float sum_rotation_x = 0.0;
 	float sum_angle_to_x = 0.0;
-	int amountOfRelevantPoints = cloud_normals->size();
+	int amount_of_relevant_points = cloud_normals->size();
 
 	for (int i = 2; i < cloud_normals->size(); i++)
 	{
-		
-		//todo remove from index
+		//check if normal has a valid value
 		if(isnan(cloud_normals->points[i].normal_x) || isnan(cloud_normals->points[i].normal_y) || isnan(cloud_normals->points[i].normal_z))
 		{
-			amountOfRelevantPoints--;
+			amount_of_relevant_points--;
 			continue;
 		}
-		
-		sum_rotation_x += atan(cloud_normals->points[i].normal_y / cloud_normals->points[i].normal_z);
 
+		//determine angle for x and y for current normal and add it to variables
+		sum_rotation_x += atan(cloud_normals->points[i].normal_y / cloud_normals->points[i].normal_z);
 		sum_angle_to_x += acos(cloud_normals->points[i].normal_x
 			/ sqrt(pow(cloud_normals->points[i].normal_x, 2)
 				+ pow(cloud_normals->points[i].normal_y, 2)
 				+ pow(cloud_normals->points[i].normal_z, 2)));
-
 	}
-	
-	Processing::angle_y = sum_angle_to_x / amountOfRelevantPoints;
 
+	//determine angle_y and angle_x by averaging
+	Processing::angle_y = sum_angle_to_x / amount_of_relevant_points;
 	Processing::angle_x = cos(Processing::angle_y) > 0 ?
-		sum_rotation_x / amountOfRelevantPoints : -(sum_rotation_x / amountOfRelevantPoints);
+		sum_rotation_x / amount_of_relevant_points : -(sum_rotation_x / amount_of_relevant_points);
 
 	cout << "angle_x: " << angle_x << endl;
 	cout << "angle_y: " << angle_y << endl;
@@ -379,9 +388,12 @@ void Processing::determineAngle(pcl::PointCloud<pcl::PointXYZ>::Ptr source_cloud
 	cout << "done..." << endl;
 }
 
+
+/**
+* Makes two scans and uses them to determine the parameters for future scans
+*/
 void Processing::positioning()
 {
-	
 	cout << "Press enter to scan" << endl;
 	cin.get();
 	string sourceCloudFile = "plain.ply";
@@ -390,35 +402,19 @@ void Processing::positioning()
 	
 	pcl::io::savePLYFileBinary("plain.ply", *plain);//save for debugging
 
-
-	cout << "Place four objects to mark the space you want to use and press enter to scan" << endl;
+	cout << "Place objects to mark the space you want to use for future scans and press enter to scan" << endl;
 	cin.get();
 	sourceCloudFile = "objects.ply";
 	const pcl::PointCloud<pcl::PointXYZ>::Ptr objects = startScanning(sourceCloudFile);
 	cout << "Done..." << endl;
 	
 	pcl::io::savePLYFileBinary("objects.ply", *objects);//save for debugging
-	
-	/*
-	string sourceCloudFile = "plain.ply";
-	pcl::PointCloud<pcl::PointXYZ>::Ptr plain = plyReader(sourceCloudFile);
-	sourceCloudFile = "toilet_paper.ply";
-	pcl::PointCloud<pcl::PointXYZ>::Ptr toilet_paper = plyReader(sourceCloudFile);
-	*/
-	
-	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = extractGround(plain, objects);
 
-	pcl::io::savePLYFileBinary("extractedGround.ply", *cloud);//save for debugging
+	//extract ground ground from first scan that is different in second scan
+	pcl::PointCloud<pcl::PointXYZ>::Ptr extractedGround = extractGround(plain, objects);
+
+	pcl::io::savePLYFileBinary("extractedGround.ply", *extractedGround);//save for debugging
 	
-	determineAngle(cloud);
-	determineRemovalParameters(transformationMatrix(cloud));
-
-	//save for debugging
-	cloud = transformationMatrix(cloud);
-	pcl::io::savePLYFileBinary("cloud.ply", *cloud);//save for debugging
-
-	
-	//plyWriter("result3out.ply", cloud);
-	//pcl::io::savePLYFileBinary("toilet_paperTransformedCut.ply", *objects);
-
+	determineAngle(extractedGround);
+	determineRemovalParameters(transformationMatrix(extractedGround));
 }
