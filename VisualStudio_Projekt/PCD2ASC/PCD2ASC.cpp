86a5b018409818cd8bc50c00e3d529deca2c7321
Michael Reno <s0565907@htw-berlin.de>
2020-01-26 12:46:56 +0100

    Added new functionality to the program, changed how ref models are used in the program

--- VisualStudio_Projekt/PCD2ASC/PCD2ASC.cpp
+++ VisualStudio_Projekt/PCD2ASC/PCD2ASC.cpp
@@ -3,8 +3,6 @@
 #include <iostream>							// for std::cout
 #include <pcl/io/pcd_io.h>					// header that contains the definitions for PCD I/O operations
 #include <pcl/filters/extract_indices.h>	// header to remove points with indices
-#include <vector>
-#include <utility>
 
 using namespace std;
 
@@ -15,6 +13,19 @@ int main ()
 	process.positioning();
 	cout << "Press enter to scan an object or type 'x' to end the program" << endl;
 	char inputChar = cin.get();
+
+	// Load reference models
+	string checkWith1 = "ball1.ply";
+	pcl::PointCloud<pcl::PointXYZ>::Ptr checkBall = process.plyReader(checkWith1); // Ref model 1 (Ball)
+	string checkWith2 = "box1.ply";
+	pcl::PointCloud<pcl::PointXYZ>::Ptr checkBox = process.plyReader(checkWith2); // Ref model 2 (Box)
+	string checkWith3 = "car1.ply";
+	pcl::PointCloud<pcl::PointXYZ>::Ptr checkCar = process.plyReader(checkWith3); // Ref model 3 (Car)
+
+	// 3 point cloud reference models to check with the source
+	ReferenceModel refModel1(checkBall);
+	ReferenceModel refModel2(checkBox);
+	ReferenceModel refModel3(checkCar);
 	
 	while(inputChar != 'x')
 	{
@@ -28,33 +39,34 @@ int main ()
 		scannedObject = process.uptRemoveBackground(scannedObject);
 		pcl::io::savePLYFileBinary("scannedObjectCut.ply", *scannedObject);
 
-		// Load reference model
-		ReferenceModel refModel(scannedObject); // source point cloud to check with the reference models
-		string checkWith1 = "ball1.ply";
-		pcl::PointCloud<pcl::PointXYZ>::Ptr checkBall = process.plyReader(checkWith1); // Ref model 1 (Ball)
-		string checkWith2 = "box1.ply";
-		pcl::PointCloud<pcl::PointXYZ>::Ptr checkBox = process.plyReader(checkWith2); // Ref model 2 (Box)
-		string checkWith3 = "car1.ply";
-		pcl::PointCloud<pcl::PointXYZ>::Ptr checkCar = process.plyReader(checkWith3); // Ref model 3 (Car)
-
-		float scores[3]; // Array to save the scoring
-
 		// Use ICP to compare source with 3 different reference models and save the result on an array
-		refModel.scoreSimilarity(checkBall);
-		scores[0] = refModel.getScoring();
-		refModel.scoreSimilarity(checkBox);
-		scores[1] = refModel.getScoring();
-		refModel.scoreSimilarity(checkCar);
-		scores[2] = refModel.getScoring();
+		refModel1.scoreSimilarity(scannedObject);
+		refModel2.scoreSimilarity(scannedObject);
+		refModel3.scoreSimilarity(scannedObject);
 
 		// Give out ICP results
 		cout << endl << "------- SCORES ------- (The closer to zero the better) -------" << endl << endl;
-		cout << "Scanned object vs Ball: " << scores[0] << endl;
-		cout << "Scanned object vs Box: " << scores[1] << endl;
-		cout << "Scanned object vs Car: " << scores[2] << endl;
+		cout << "Scanned object vs Ball: " << refModel1.getScoring() << endl;
+		cout << "Scanned object vs Box: " << refModel2.getScoring() << endl;
+		cout << "Scanned object vs Car: " << refModel3.getScoring() << endl;
 
+		if (refModel1.getScoring() < refModel2.getScoring() && refModel1.getScoring() < refModel3.getScoring())
+		{
+			cout << endl;
+			cout << "Your scanned object is closest to a ball, with a score of " << refModel1.getScoring() << endl;
+		}
+		else if (refModel2.getScoring() < refModel1.getScoring() && refModel2.getScoring() < refModel3.getScoring())
+		{
+			cout << endl;
+			cout << "Your scanned object is closest to a box, with a score of " << refModel2.getScoring() << endl;
+		}
+		else
+		{
+			cout << endl;
+			cout << "Your scanned object is closest to a car, with a score of " << refModel3.getScoring() << endl;
+		}
+		
 		cout << "Press enter to scan an object or type 'x' to end the program" << endl;
 		inputChar = cin.get();
-		
 	}
 }
