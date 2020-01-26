fbdee2aae78f44ea51e0950951f02b04c3f328a5
s0558411 <minh.do@student.htw-berlin.de>
2020-01-26 10:51:26 +0100

    Update script_cmake.sh

--- script_cmake.sh
+++ script_cmake.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
 
-#Zielpfad hier angeben
-BUILDPATH=/home/pi
+#Zielpfad 
+BUILDPATH=/home/pi/librealsense
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 cd $BUILDPATH
-rm -r final_exe
-mkdir -p final_exe
-cd $BUILDPATH/final_exe
-rm -r build
-mkdir -p build
+rm -r swe_project
+mkdir -p swe_project
+cd $BUILDPATH/swe_project
 
-cp $SCRIPTPATH/VisualStudio_Projekt/PCD2ASC/PCD2ASC.cpp /$BUILDPATH/final_exe
-cp $SCRIPTPATH/VisualStudio_Projekt/PCD2ASC/Processing.cpp /$BUILDPATH/final_exe
-cp $SCRIPTPATH/VisualStudio_Projekt/PCD2ASC/ReferenceModel.cpp /$BUILDPATH/final_exe
-cp $SCRIPTPATH/VisualStudio_Projekt/PCD2ASC/CRealsenseScan.cpp /$BUILDPATH/final_exe
+cp $SCRIPTPATH/VisualStudio_Projekt/PCD2ASC/PCD2ASC.cpp /$BUILDPATH/swe_project
+cp $SCRIPTPATH/VisualStudio_Projekt/PCD2ASC/Processing.cpp /$BUILDPATH/swe_project
+cp $SCRIPTPATH/VisualStudio_Projekt/PCD2ASC/ReferenceModel.cpp /$BUILDPATH/swe_project
+cp $SCRIPTPATH/VisualStudio_Projekt/PCD2ASC/CRealsenseScan.cpp /$BUILDPATH/swe_project
+cp $SCRIPTPATH/VisualStudio_Projekt/PCD2ASC/example.hpp /$BUILDPATH/swe_project
+cp $SCRIPTPATH/VisualStudio_Projekt/PCD2ASC/CMakelists.txt /$BUILDPATH
 
-cp $SCRIPTPATH/VisualStudio_Projekt/PCD2ASC/Processing.h /$BUILDPATH/final_exe
-cp $SCRIPTPATH/VisualStudio_Projekt/PCD2ASC/ReferenceModel.h /$BUILDPATH/final_exe
-cp $SCRIPTPATH/VisualStudio_Projekt/PCD2ASC/CRealsenseScan.h /$BUILDPATH/final_exe
+cp $SCRIPTPATH/VisualStudio_Projekt/PCD2ASC/Processing.h /$BUILDPATH/swe_project
+cp $SCRIPTPATH/VisualStudio_Projekt/PCD2ASC/ReferenceModel.h /$BUILDPATH/swe_project
+cp $SCRIPTPATH/VisualStudio_Projekt/PCD2ASC/CRealsenseScan.h /$BUILDPATH/swe_project
 
 #CMakeLists.txt wird erstellt
 echo '
-cmake_minimum_required(VERSION 2.8 FATAL_ERROR)
+# License: Apache 2.0. See LICENSE file in root directory.
+# Copyright(c) 2019 Intel Corporation. All Rights Reserved.
+#  minimum required cmake version: 3.1.0
+cmake_minimum_required(VERSION 3.1.0)
 
 project(PCD2ASC)
+
 
 find_package(PCL 1.2 REQUIRED)
 
@@ -34,12 +38,15 @@ link_directories(${PCL_LIBRARY_DIRS})
 add_definitions(${PCL_DEFINITIONS})
 
 
-add_executable (PCD2ASC PCD2ASC.cpp Processing.cpp ReferenceModel.cpp Processing.h ReferenceModel.h CRealsenseScan.cpp CRealsenseScan.h)
-target_link_libraries (PCD2ASC ${PCL_LIBRARIES})
+add_executable (PCD2ASC CRealsenseScan.cpp CRealsenseScan.h PCD2ASC.cpp Processing.cpp Processing.h ReferenceModel.cpp ReferenceModel.h example.hpp)
+set_property(TARGET PCD2ASC PROPERTY CXX_STANDARD 11)
+target_link_libraries (PCD2ASC ${PCL_LIBRARIES} ${DEPENDENCIES})
+include_directories(rs-pointcloud ../examples/)
+
 ' > CMakeLists.txt
 
-cd $BUILDPATH/final_exe/build
+cd $BUILDPATH
 
-
-cmake ..
+cmake .. 
 make
+
